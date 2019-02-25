37026d5f843bc5b1abaff167db7d7088b0b0d9a6
Mika88 <mikaspivak@gmail.com>
2019-02-25 12:34:43 -0800

    Create sessions controller. Add new, create and destroy actions in sessions. Add /login and /logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :arrangements
   resources :users
+  get '/login', :to => 'sessions#new'
+  post '/sessions', :to => 'sessions#create'
+  get '/logout', :to => 'sessions#destroy'
 end
