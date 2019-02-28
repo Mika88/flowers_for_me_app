534b36c448797ad60d5c5a0ca95ad2ab14990fce
Mika88 <mikaspivak@gmail.com>
2019-02-28 07:41:36 -0800

    Add nested routes (resources :orders) to resources :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'application#home'
   resources :arrangements
-  resources :users
+  resources :users do
+    resources :orders
+  end
   get '/login', :to => 'sessions#new'
   post '/sessions', :to => 'sessions#create'
   get '/logout', :to => 'sessions#destroy'
