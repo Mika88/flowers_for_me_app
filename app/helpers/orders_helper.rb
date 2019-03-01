c63bfa066d954b97a22c54707512033210f246d4
Mika88 <mikaspivak@gmail.com>
2019-03-01 09:11:40 -0800

    Create #delivery_date in orders_helper to handle date_select input in orders#new

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,4 +2,10 @@ module OrdersHelper
     def created_time(order)
       order.created_at.strftime("%A, %b %e at %l:%M %p (UTC)")
     end
+
+    def delivery_date
+       delivery_hash = eval(@order.delivery_day)
+       "#{delivery_hash[2]}/#{delivery_hash[3]}/#{delivery_hash[1]}"
+    end
+
 end
