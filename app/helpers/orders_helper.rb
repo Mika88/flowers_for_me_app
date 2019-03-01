f383051a8f617e85a3754677732a47748897c558
Mika88 <mikaspivak@gmail.com>
2019-02-28 19:52:02 -0800

    Refactor #created_time in orders_helper and fix a typo in #created_time in orders#show

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,5 +1,5 @@
 module OrdersHelper
     def created_time(order)
-      order.created_at.strftime("%A, %b %e, at %l:%M %p (UTC)")
+      order.created_at.strftime("%A, %b %e at %l:%M %p (UTC)")
     end
 end
