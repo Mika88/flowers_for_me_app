87d3caa25a0c44636ab5b24aacf0001c443b0488
Mika88 <mikaspivak@gmail.com>
2019-03-04 13:18:13 -0800

    Add find_or_create_by_omniauth method to user.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,10 +18,4 @@ class SessionsController < ApplicationController
         session.delete :user_id
         redirect_to root_path
     end
-    
-    private
-    
-    def auth
-      request.env['omniauth.auth']
-    end
 end
