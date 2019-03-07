abb0b1b33c4eea44221b2d2ee1eec9bc03416a4e
Mika88 <mikaspivak@gmail.com>
2019-03-07 14:50:12 -0800

    Add helper methods to application_helper to control sign_up and loin flows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
-    
+  def login_flow
+    if logged_in? 
+      link_to "Login", user_path(current_user) 
+    else
+      link_to "Login", '/login' 
+    end 
+  end
+  
+  def sign_up_flow
+    if logged_in? 
+      link_to "Sign Up", user_path(current_user) 
+    else
+      link_to "Sign Up", new_user_path  
+    end 
+  end
 end
