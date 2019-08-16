7f7a984fe07fdc67ca4451604ca96a4df4094a3e
Mika88 <mikaspivak@gmail.com>
2019-08-16 11:56:50 -0700

    Render the user's orders in their show page and transfer the orders_helper.rb methods to Order model so they could be used on the user show page

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,20 +1,3 @@
 module OrdersHelper
-    def created_time
-      @order.created_at.strftime("%b %e, %Y at %l:%M %p (UTC)")
-    end
-
-    def delivery_date
-      case @order.delivery_day
-      when "in 10 days - free"
-        ten_days = @order.created_at + 10.day
-        ten_days.strftime("%b %e, %Y")
-      when "in a week - $5"
-        week = @order.created_at + 7.day
-        week.strftime("%b %e, %Y")
-      when "in two days - $10"
-        two_days = @order.created_at + 2.day
-        two_days.strftime("%b %e, %Y")
-      end 
-    end
 end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -15,4 +15,22 @@ class Order < ApplicationRecord
          items_price
       end 
     end
+
+    def created_time
+      self.created_at.strftime("%b %e, %Y at %l:%M %p (UTC)")
+    end
+
+    def delivery_date
+      case self.delivery_day
+      when "in 10 days - free"
+        ten_days = self.created_at + 10.day
+        ten_days.strftime("%b %e, %Y")
+      when "in a week - $5"
+        week = self.created_at + 7.day
+        week.strftime("%b %e, %Y")
+      when "in two days - $10"
+        two_days = self.created_at + 2.day
+        two_days.strftime("%b %e, %Y")
+      end 
+    end
 end
