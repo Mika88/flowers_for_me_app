c6957a3c2e69dc6cd698711031c6642f1f653608
Mika88 <mikaspivak@gmail.com>
2019-10-20 12:05:14 -0700

    Move total_price and delivery_date from the orders model to the helpers/order.rb file, since they should be helper methods. Change the methods calls in the order show view

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,5 +2,31 @@ module OrdersHelper
   def created_time(order)
     order.created_at.strftime("%b %e, %Y at %l:%M %p (UTC)")
   end
+
+  def total_price(order)
+    items_price = order.arrangement.price * order.quantity
+    case order.delivery_day
+    when "in a week - $5"
+       items_price + 5
+    when "in two days - $10"
+       items_price + 10
+    else 
+       items_price
+    end 
+  end
+
+  def delivery_date(order)
+    case order.delivery_day
+    when "in 10 days - free"
+      ten_days = order.created_at + 10.day
+      ten_days.strftime("%b %e, %Y")
+    when "in a week - $5"
+      week = order.created_at + 7.day
+      week.strftime("%b %e, %Y")
+    when "in two days - $10"
+      two_days = order.created_at + 2.day
+      two_days.strftime("%b %e, %Y")
+    end 
+  end
 end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,32 +3,4 @@ class Order < ApplicationRecord
     belongs_to :arrangement
     
     validates :delivery_day, presence: true
-    
-    def total_price
-      items_price = self.arrangement.price * self.quantity
-      case self.delivery_day
-      when "in a week - $5"
-         items_price + 5
-      when "in two days - $10"
-         items_price + 10
-      else 
-         items_price
-      end 
-    end
-
-    
-
-    def delivery_date
-      case self.delivery_day
-      when "in 10 days - free"
-        ten_days = self.created_at + 10.day
-        ten_days.strftime("%b %e, %Y")
-      when "in a week - $5"
-        week = self.created_at + 7.day
-        week.strftime("%b %e, %Y")
-      when "in two days - $10"
-        two_days = self.created_at + 2.day
-        two_days.strftime("%b %e, %Y")
-      end 
-    end
 end
